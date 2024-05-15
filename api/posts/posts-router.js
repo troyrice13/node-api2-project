103cb583b8416326c336b24011fa53ec7f173d98
Troy Rice <troyanthonyrice@gmail.com>
2024-05-15 14:50:16 -0700

    GET passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,9 +1,34 @@
 // implement your posts router here
 const express = require ('express')
-
 const router = express.Router();
+const Post = require('./posts-model')
 
+router.get('/', (req, res) => {
+    Post.find()
+        .then(posts => {
+            res.json(posts)
+        })
+});
 
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+router.get('/:id/comments', (req, res) => {
+
+});
 
 
 
--- api/server.js
+++ api/server.js
@@ -1,13 +1,14 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require('express');
+const postsRouter = require('./posts/posts-router');
 const server = express();
 
 server.use(express.json());
 
-const postsRouter = require('./posts/posts-router');
 
-// server.use('./api/posts/posts-router', postsRouter)
+
+server.use('/api/posts', postsRouter)
 
 server.use('/', (req, res) => {
     res.status(200).send('Hello from express')
